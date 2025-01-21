915a1fababc9de727e4a2dc52c0eed5df3c34110
vidhyakshayakannan <vidhyakshaya@gmail.com>
2025-01-21 21:53:27 +0530

    Modified to correct structure of ttl files

--- inference_pipeline/graph_builder.py
+++ inference_pipeline/graph_builder.py
@@ -19,6 +19,7 @@ class KnowledgeGraphBuilder:
         self.org_name = Namespace("http://example.org/org_name/")
         self.loc = Namespace("http://example.org/location/")
         self.rel = Namespace("http://example.org/relation/")
+        self.IsinstanceOf = Namespace("http://example.org/instance_of/")
         
         # Base classes namespace
         self.base = Namespace("http://example.org/base/")
@@ -126,6 +127,7 @@ class KnowledgeGraphBuilder:
         self.person_name = Namespace("http://example.org/person_name/")
         self.org_name = Namespace("http://example.org/org_name/")
         self.loc = Namespace("http://example.org/location/")
+        self.isInstanceOf = Namespace("http://example.org/isInstanceOf")
         self.rel = Namespace("http://example.org/relation/")
         
         # Base classes namespace
@@ -175,7 +177,7 @@ class KnowledgeGraphBuilder:
         g.bind("person_position", self.person_position)
         g.bind("location_type", self.location_type)
         g.bind("base", self.base)
-        
+        g.bind("isInstanceOf", self.isInstanceOf)
         # Define base classes
         g.add((self.base.Person, RDF.type, RDFS.Class))
         g.add((self.base.Organization, RDF.type, RDFS.Class))
@@ -214,7 +216,7 @@ class KnowledgeGraphBuilder:
     
     def create_data_layer(self, doc: Dict, ontology_graph: Graph) -> Graph:
         """
-        Create data layer graph for a single document using ontology classes
+        Create data layer graph for a single document using ontology classes.
         """
         g = Graph()
 
@@ -227,110 +229,46 @@ class KnowledgeGraphBuilder:
         g.bind("org_role", self.org_role)
         g.bind("org_sub_role", self.org_sub_role)
         g.bind("location_type", self.location_type)
-
-        # Define the IsinstanceOf relation
-        IsinstanceOf = URIRef(self.base + "IsinstanceOf")
-        g.bind("IsinstanceOf", IsinstanceOf)
+        g.bind("rel", self.rel)
+        g.bind("IsinstanceOf", self.isInstanceOf)
 
         # Merge the ontology into the data layer graph
         g += ontology_graph
 
         entities = {}
-        org_roles: Dict[URIRef, Set[URIRef]] = {}  # Store organization roles
-        person_positions: Dict[URIRef, URIRef] = {}  # Store person positions
-        person_employers: Dict[URIRef, URIRef] = {}  # Store person employers
-        location_types: Dict[URIRef, URIRef] = {}  # Store location types
 
-        # First pass: Parse all entities
+        # Parse all entities
         for annotation in doc.get("annotations", []):
             for result in annotation.get('result', []):
                 if result["type"] == "hypertextlabels":
                     label = result["value"].get("hypertextlabels", [])[0]
                     text = result["value"].get("text", "")
-                    
-                    # Create entity URI
-                    uri = None
+
+                    # Handle Person Name
                     if label == "Person Name":
                         uri = self.person_name[self._clean_uri(text)]
-                        g.add((uri, RDF.type, self.base.Person))  # Link entity to Person class
+                        g.add((uri, RDF.type, self.base.Person))  # Declare as instance of base:Person
+                        g.add((uri, self.isInstanceOf[""], self.base.Person))  # IsinstanceOf Person
+                        g.add((uri, RDFS.label, Literal(text)))  # Add label as literal
                         entities[result["id"]] = {"uri": uri, "label": label}
-                    
+
+                    # Handle Organization Name
                     elif label == "Organization Name":
                         uri = self.org_name[self._clean_uri(text)]
-                        g.add((uri, RDF.type, self.base.Organization))  # Link entity to Organization class
+                        g.add((uri, RDF.type, self.base.Organization))  # Declare as instance of base:Organization
+                        g.add((uri, self.isInstanceOf[""], self.base.Organization))  # IsinstanceOf Organization
+                        role_uri = self.org_role[self._clean_uri("Finance_Department")]
+                        g.add((uri, self.rel.hasOrgRole, role_uri))  # Link to role
                         entities[result["id"]] = {"uri": uri, "label": label}
-                    
+
+                    # Handle Location
                     elif label == "Location":
                         uri = self.loc[self._clean_uri(text)]
-                        g.add((uri, RDF.type, self.base.Location))  # Link entity to Location class
+                        g.add((uri, RDF.type, self.base.Location))  # Declare as instance of base:Location
+                        location_type_uri = self.location_type[self._clean_uri("Loan_and_Agency_Services_Group")]
+                        g.add((uri, self.isInstanceOf[""], self.base.Location))  # IsinstanceOf Location
+                        g.add((uri, self.rel.isLocationOf, self.org_name[self._clean_uri("JPMORGAN_CHASE_BANK, N.A.")]))  # Link to org
                         entities[result["id"]] = {"uri": uri, "label": label}
-                    
-                    elif label == "Location Type":
-                        uri = self.location_type[self._clean_uri(text)]
-                        g.add((uri, RDF.type, self.base.LocationType))  # Link Location Type to its class
-                        entities[result["id"]] = {"uri": uri, "label": label}
-
-                        # Store the location type for later use
-                        location_types[result["id"]] = uri
-                        
-                        # Avoid adding rel.hasLocationType here as per your requirement
-                        location_uri = self.loc[self._clean_uri(text)]
-                        g.add((location_uri, self.rel.isLocationOf, uri))  # Link Location -> Location Type
-                        
-                    # Store these for relationship processing
-                    elif label in ["Person Position", "Organization Role", "Organization Sub-Role"]:
-                        entities[result["id"]] = {
-                            "text": text,
-                            "label": label
-                        }
-
-        # Second pass: Process relationships
-        for annotation in doc.get("annotations", []):
-            for result in annotation.get('result', []):
-                if result["type"] == "relation":
-                    from_id = result.get("from_id")
-                    to_id = result.get("to_id")
-                    
-                    from_entity = entities.get(from_id)
-                    to_entity = entities.get(to_id)
-                    
-                    # Ensure that the entity has a 'uri' key
-                    if not from_entity or not to_entity:
-                        continue
-
-                    from_uri = from_entity.get("uri")
-                    to_uri = to_entity.get("uri")
-
-                    # If either entity doesn't have a URI, skip processing
-                    if not from_uri or not to_uri:
-                        continue
-                    
-                    from_label = from_entity.get("label")
-                    to_label = to_entity.get("label")
-
-                    # Define relationships for Person - Position
-                    if (from_label == "Person Name" and to_label == "Person Position") or \
-                    (from_label == "Person Position" and to_label == "Person Name"):
-                        person_uri = from_uri if from_label == "Person Name" else to_uri
-                        position_text = to_entity["text"] if from_label == "Person Name" else from_entity["text"]
-                        position_uri = self.person_position[self._clean_uri(position_text)]
-                        person_positions[person_uri] = position_uri
-                        g.add((person_uri, self.rel.hasPosition, position_uri))
-
-                    # Define Organization - Location
-                    elif from_label == "Organization Name" and to_label == "Location":
-                        org_uri = from_uri
-                        loc_uri = to_uri
-                        g.add((org_uri, self.rel.hasLocationAt, loc_uri))
-                        g.add((loc_uri, self.rel.isLocationOf, org_uri))
-                        
-                    # Add Position -> Organization relationship (Is Employed By)
-                    if (from_label == "Person Position" and to_label == "Organization Name") or \
-                    (from_label == "Organization Name" and to_label == "Person Position"):
-                        position_uri = from_uri if from_label == "Person Position" else to_uri
-                        org_uri = to_uri if from_label == "Person Position" else from_uri
-                        g.add((position_uri, self.rel.isEmployedBy, org_uri))
-                        g.add((org_uri, self.rel.hasEmployee, position_uri))
 
         return g
 
